010abc48822520da35953823a0f5bd1fecc0f84a
Geovanni <geovannicunha@gmail.com>
2024-09-28 17:53:59 -0300

    oi

--- ReservasVoos.py
+++ ReservasVoos.py
@@ -1,1 +1,16 @@
-1- Crie um programa que simule um sistema básico de reserva de voos. O programa deve permitir que o usuário adicione reservas de voos enquanto desejar. ##Cada reserva deve incluir o nome do passageiro e o destino. Ao final, o programa deve listar todas as reservas feitas.
+#Crie um programa que simule um sistema básico de reserva de voos. O programa deve permitir que o usuário
+#adicione reservas de voos enquanto desejar. Cada reserva deve incluir o nome do passageiro e o destino. Ao final, o
+#programa deve listar todas as reservas feitas.
+
+nomes = []
+destinos = []
+
+continua = "s"
+while continua == "s":
+    nome = input("Digite um nome: ")
+    nomes.append(nome)
+    destino = input("Digite o destino: ")
+    destinos.append(destino)
+    continua = input("Deseja continuar?: ")
+for i in range(len(nomes)):
+    print(f"Nome: {nomes[i]}, Destino: {destinos[i]} ")
---
+++ Usuario5numeros.py
@@ -0,0 +1,8 @@
+# Crie um programa que solicite ao usuário 5 números e ao final imprima a soma de todos eles.
+
+numeros = []
+soma= []
+
+
+
+
